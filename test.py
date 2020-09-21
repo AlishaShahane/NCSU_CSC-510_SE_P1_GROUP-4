e23933ab959c1d353287a6389b82c517dbc3f1e4
shruti-k <shrutik1405@gmail.com>
2020-09-21 19:22:23 -0400

    Update test.py

--- test.py
+++ test.py
@@ -9,14 +9,6 @@ class TestAgent(unittest.TestCase):
     def setUp(self):        
         print("Initialised unit test")
 
-    # Unit test test two functions on a single line
-    def test_nodal_precession(self):
-        #Checks the graph formed
-        pp1 = pp.Pandemic(0.01, 0.6, range(20, 30, 1))
-        expected_state = 324
-        returned_state = len(pp1.getGraph("Data/pandemic_data.txt").vs)
-        self.assertEqual(expected_state,returned_state)
-
 if __name__ == '__main__':
     main = TestAgent()
 
