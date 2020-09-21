f4265d8d8fcd1f27d1299468a5a7754dbab4a2e2
shruti-k <shrutik1405@gmail.com>
2020-09-21 19:23:15 -0400

    Update test.py

--- test.py
+++ test.py
@@ -8,6 +8,10 @@ class TestAgent(unittest.TestCase):
 
     def setUp(self):        
         print("Initialised unit test")
+    
+    def test_nodal_precession(self):
+        pp1 = pp.Pandemic(0.01, 0.6, range(20, 30, 1))
+        self.assertEqual([0.01, 0.6],[pp1.beta, pp1.delta])
 
 if __name__ == '__main__':
     main = TestAgent()
